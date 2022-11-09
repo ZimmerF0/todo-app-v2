717d1c4a2f90e3f461b55aa61b8410d3d16f588b
Kishkin Vitalii <dimpla30@gmail.com>
2022-11-09 12:00:45 +0300

    completing a task with a double click

--- src/App.js
+++ src/App.js
@@ -5,14 +5,20 @@ import './App.css'
 
 function App() {
   const [todos,setTodos] = useState([])
+
   const addTodoHandler = (text) => {
     setTodos([...todos, text])
+  }
+
+  const deleteTodoHandler = (index) => {
+setTodos(todos.filter((_, idx) => idx !== index))
   }
 
   return <div className="App">
     <h1>Todo App</h1>
     < TodoForm addTodo={addTodoHandler} />
-    <TodoList todos={todos} />
+    <TodoList todos={todos} deleteTodo={deleteTodoHandler} />
+   
   </div>
 }
 
